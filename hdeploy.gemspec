79514f9c193488b0184c223ad7ab14b95e413a0f
Patrick Viet <patrick.viet@gmail.com>
2016-09-12 21:45:24 -0400

    new config method, better

--- hdeploy.gemspec
+++ hdeploy.gemspec
@@ -5,20 +5,22 @@ Gem::Specification.new do |s|
   s.name          = "hdeploy"
   s.version       = HDeploy::VERSION
   s.authors       = ["Patrick Viet"]
-  s.email         = ["patrick.viet@gmail.com","it-admin@getyourguide.com"]
+  s.email         = ["patrick.viet@gmail.com"]
   s.description   = %q{HDeploy tool}
   s.summary       = %q{no summary}
-  s.homepage      = "https://github.com/getyourguide/hdeploy"
+  s.homepage      = "https://github.com/hdeploy/hdeploy"
 
   s.files         = `git ls-files`.split($/)
   s.executables   = s.files.grep(%r{^bin/}).map{ |f| File.basename(f) }
   s.test_files    = s.files.grep(%r{^(test|spec|features)/})
   s.require_paths = ["lib"]
 
-  s.add_runtime_dependency 'json',    '~> 1.7.7'
-  s.add_runtime_dependency 'curb',    '~> 0.8.5'
-  s.add_runtime_dependency 'sinatra', '~> 1.4.5'
-  s.add_runtime_dependency 'inifile',   '~> 2.0.2'  
-  s.add_runtime_dependency 'cassandra-driver',   '~> 2.0.1'
-  s.add_runtime_dependency 'eventmachine', '~> 1.0.7'
+  s.add_runtime_dependency 'json',    '>= 1.7.7'
+  s.add_runtime_dependency 'curb',    '>= 0.8.5'
+  s.add_runtime_dependency 'sinatra', '>= 1.4.5'
+  s.add_runtime_dependency 'inifile',   '>= 2.0.2'  
+  s.add_runtime_dependency 'cassandra-driver',   '>= 2.0.1'
+  s.add_runtime_dependency 'eventmachine', '>= 1.0.7'
+
+  s.add_development_dependency 'pry'
 end
--- lib/hdeploy/config.rb
+++ lib/hdeploy/config.rb
@@ -1,52 +1,50 @@
 require 'inifile'
-require 'singleton'
 
 module HDeploy
   class Config
-    include Singleton
 
-    def initialize(path = '/opt/hdeploy/etc')
+    @@instance = nil
+
+    def initialize(path)
+      @path = path
+      @conf = {}
+    end
+
+    def self.instance(path = '/opt/hdeploy/etc')
+      @@instance ||= new(path)
+    end
 
     # -------------------------------------------------------------------------
-    def load_file(conf_type)
+    def _load_file(ctype)
     
-      # conf_type should be one of these:
-      # build, api, node
+      # conf type should be one of these:
+      raise "No such file authorized to load '#{ctype}'" unless [:node, :build, :api].include? ctype
       # the config file should NOT be writable by any other user than chef - and it will also deny symlinks
       # for general security
 
-      cfile = File.join(@path, "hdeploy_#{conf_type}.ini")
+      cfile = File.join(@path, "hdeploy_#{ctype}.conf")
       raise "unable to find conf file #{cfile}" unless File.exists? cfile
       
       st = File.stat(cfile)
       raise "config file #{cfile} must not be a symlink" if File.symlink?(cfile)
       raise "config file #{cfile} must be a regular file" unless st.file?
-      raise "config file #{cfile} must have uid 0" unless st.uid == 0
+      raise "config file #{cfile} must have uid 0" unless st.uid == 0 or Process.uid != 0
       raise "config file #{cfile} must not allow group/others to write" unless sprintf("%o", st.mode) =~ /^100[46][04][04]/
       
       # Seems we have checked everything. Woohoo!
-      @cfile[conf_type] = cfile
-      
+      @conf[ctype.to_sym] = IniFile.load(cfile).to_h
     end
 
     # -------------------------------------------------------------------------
-    def reload(conf_type = nil)
-      if conf_type
-        conf[conf_type] = IniFile.load(@cfile[conf_type]).to_h
-      else
-        # load all
-        @cfile.each do |t,f|
-          conf[t] = IniFile.load(@cfile).to_h
-        end
+    def reload
+      @conf.keys.each do |t|
+        _load_file(t)
       end
-
-
-
-
-      @conf[t] = IniFile.load(@cfile).to_h
     end
 
-    def method_missing
-
+    # -------------------------------------------------------------------------
+    def method_missing(method, *args, &block)
+      @conf[method] ||= _load_file(method)  
+    end
   end
 end
